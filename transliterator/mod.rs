941dc6271bad85fd4a45ae986c25c3ec7855809a
DarkEld3r <stanislav.tkach@gmail.com>
2014-11-28 01:17:02 +0200

    HashMap.

--- transliterator/mod.rs
+++ transliterator/mod.rs
@@ -1,7 +1,11 @@
-// TODO:
-// HashMap
+use std::collections::HashMap;
 
 fn test()
 {
   println!("transliterator!")
+
+  let mut test = HashMap::new();
+
+  test.insert(('a', 'a'), 'a');
+  test.insert(('a', 'b'), 'b');
 }
