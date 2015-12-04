3c4ebed2802ce5a4a162c6ccbeb8f45bf81c6f2c
DarkEld3r <stanislav.tkach@gmail.com>
2015-12-05 01:42:28 +0200

    GUI - work in progress

--- gui/main.rs
+++ gui/main.rs
@@ -1,5 +1,20 @@
 extern crate translator;
 
+extern crate piston;
+extern crate piston_window;
+extern crate glutin_window;
+
+use piston::event_loop::{Events, EventLoop};
+
 fn main() {
-	println!("Hello, GUI world!")
+    let window: glutin_window::GlutinWindow = piston_window::WindowSettings::new("Hello Conrod", [800, 600])
+//        .opengl(opengl)
+        .exit_on_esc(true)
+        .samples(4)
+        .build()
+        .unwrap();
+
+    let event_iter = window.events().ups(60).max_fps(60);
+    for _event in event_iter {
+    }
 }
