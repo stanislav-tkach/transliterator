f8f7ef8f157081261d19cc035c791e53bac6227d
DarkEld3r <stanislav.tkach@gmail.com>
2016-01-18 01:51:42 +0200

    Rustfmt

--- cui/main.rs
+++ cui/main.rs
@@ -1,15 +1,16 @@
 extern crate translator;
 
 fn main() {
-	let args = std::env::args();
-	if args.len() < 2 {
-		println!("Usage:");
-		println!("{} <text to translate>", args.take(1).last().unwrap_or("transliterator".to_string()));
-		return;
-	}
+    let args = std::env::args();
+    if args.len() < 2 {
+        println!("Usage:");
+        println!("{} <text to translate>",
+                 args.take(1).last().unwrap_or("transliterator".to_string()));
+        return;
+    }
 
-	let text = args.skip(1).fold(String::new(), |result, param| result + " " + &param).trim_left().to_string();
+    let text = args.skip(1).fold(String::new(), |result, param| result + " " + &param).trim_left().to_string();
 
-	let translator = translator::Translator::new();
-	println!("{}", translator.translate(&text));
+    let translator = translator::Translator::new();
+    println!("{}", translator.translate(&text));
 }
--- gui/main.rs
+++ gui/main.rs
@@ -7,7 +7,10 @@ use piston::event_loop::{Events, EventLoop};
 
 fn main() {
     let window: piston_window::PistonWindow = piston_window::WindowSettings::new("Hello Conrod", [800, 600])
-        .exit_on_esc(true).vsync(true).build().unwrap();
+                                                  .exit_on_esc(true)
+                                                  .vsync(true)
+                                                  .build()
+                                                  .unwrap();
 
     for event in window.ups(60) {
     }
