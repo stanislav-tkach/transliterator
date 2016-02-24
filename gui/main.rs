d0ac36dc80b5e99f5858fdc0a99676c0dd9484f2
DarkEld3r <stanislav.tkach@gmail.com>
2016-02-25 01:33:20 +0200

    Work in progress

--- gui/main.rs
+++ gui/main.rs
@@ -2,9 +2,12 @@ extern crate translator;
 
 extern crate piston;
 extern crate piston_window;
+extern crate conrod;
+extern crate find_folder;
 
-use piston_window::{PistonWindow, WindowSettings};
+use piston_window::{PistonWindow, WindowSettings, Glyphs};
 use piston::event_loop::{Events, EventLoop};
+use conrod::{Theme, Canvas};
 
 fn main() {
     let window: PistonWindow = WindowSettings::new("Hello Conrod", [800, 600])
@@ -13,12 +16,21 @@ fn main() {
                                                   .build()
                                                   .unwrap();
 
+    let ui = {
+        let assets_path = find_folder::Search::KidsThenParents(3, 5).for_folder("assets").unwrap();
+        let font_path = assets_path.join("NotoSans-Regular.ttf");
+        let theme = Theme::default();
+        let glyph_cache = Glyphs::new(&font_path, window.factory.borrow().clone());
+        conrod::Ui::new(glyph_cache.unwrap(), theme)
+    };
+
     for event in window.ups(60) {
-        self.ui.set_widgets(|ui| {
+        ui.set_widgets(|ui| {
             Canvas::new()
                 .pad(30.)
                 .color(conrod::color::rgb(0.2, 0.35, 0.45))
                 .scroll_kids()
                 .set(CANVAS, ui);
+        });
     }
 }
