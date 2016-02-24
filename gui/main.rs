58337b441f2b6a89cb735367958e618c861678f4
DarkEld3r <stanislav.tkach@gmail.com>
2016-02-25 01:36:44 +0200

    Build fix

--- gui/main.rs
+++ gui/main.rs
@@ -2,12 +2,13 @@ extern crate translator;
 
 extern crate piston;
 extern crate piston_window;
-extern crate conrod;
+#[macro_use] extern crate conrod;
 extern crate find_folder;
 
 use piston_window::{PistonWindow, WindowSettings, Glyphs};
-use piston::event_loop::{Events, EventLoop};
-use conrod::{Theme, Canvas};
+use piston::event_loop::{EventLoop};
+use conrod::{Theme, Ui, Canvas, Widget};
+use conrod::color::Colorable;
 
 fn main() {
     let window: PistonWindow = WindowSettings::new("Hello Conrod", [800, 600])
@@ -16,12 +17,12 @@ fn main() {
                                                   .build()
                                                   .unwrap();
 
-    let ui = {
+    let mut ui = {
         let assets_path = find_folder::Search::KidsThenParents(3, 5).for_folder("assets").unwrap();
         let font_path = assets_path.join("NotoSans-Regular.ttf");
         let theme = Theme::default();
         let glyph_cache = Glyphs::new(&font_path, window.factory.borrow().clone());
-        conrod::Ui::new(glyph_cache.unwrap(), theme)
+        Ui::new(glyph_cache.unwrap(), theme)
     };
 
     for event in window.ups(60) {
