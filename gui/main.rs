a1de721e8e1b8d62aad24a805b0bcb61f3609ca8
DarkEld3r <stanislav.tkach@gmail.com>
2016-02-26 00:30:17 +0200

    Fmt

--- gui/main.rs
+++ gui/main.rs
@@ -2,20 +2,21 @@ extern crate translator;
 
 extern crate piston;
 extern crate piston_window;
-#[macro_use] extern crate conrod;
+#[macro_use]
+extern crate conrod;
 extern crate find_folder;
 
 use piston_window::{PistonWindow, WindowSettings, Glyphs, UpdateEvent};
-use piston::event_loop::{EventLoop};
+use piston::event_loop::EventLoop;
 use conrod::{Theme, Ui, Canvas, TextBox, Widget, Sizeable};
 use conrod::color::{self, Colorable};
 
 fn main() {
     let window: PistonWindow = WindowSettings::new("Hello Conrod", [800, 600])
-                                                  .exit_on_esc(true)
-                                                  .vsync(true)
-                                                  .build()
-                                                  .unwrap();
+                                   .exit_on_esc(true)
+                                   .vsync(true)
+                                   .build()
+                                   .unwrap();
 
     let mut ui = {
         let assets_path = find_folder::Search::KidsThenParents(3, 5).for_folder("assets").unwrap();
@@ -28,11 +29,12 @@ fn main() {
     for w in window.ups(60) {
         ui.handle_event(&w);
 
-        w.update(|_| ui.set_widgets(|ui| {
-            let background_color = color::rgb(0.2, 0.35, 0.45);
-            let mut text = "".to_owned();
+        w.update(|_| {
+            ui.set_widgets(|ui| {
+                let background_color = color::rgb(0.2, 0.35, 0.45);
+                let mut text = "".to_owned();
 
-            Canvas::new()
+                Canvas::new()
                     .pad(30.)
                     .color(background_color)
                     .scroll_kids()
@@ -42,16 +44,17 @@ fn main() {
                     .font_size(20)
                     .w_h(420.0, 40.0)
                     .color(background_color.invert())
-                    .react(|_string: &mut String|{})
+                    .react(|_string: &mut String| {})
                     .set(INPUT_TEXT, ui);
 
-            TextBox::new(&mut text)
+                TextBox::new(&mut text)
                     .font_size(20)
                     .w_h(320.0, 40.0)
                     .color(background_color.invert())
-                    .react(|_string: &mut String|{})
-                .set(OUTPUT_TEXT, ui);
-        }));
+                    .react(|_string: &mut String| {})
+                    .set(OUTPUT_TEXT, ui);
+            })
+        });
 
         w.draw_2d(|context, graphic| ui.draw_if_changed(context, graphic));
     }
