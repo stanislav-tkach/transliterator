b849fb152aa6d202f9eea5e73821c7946eccd9e4
DarkEld3r <stanislav.tkach@gmail.com>
2016-05-02 03:13:12 +0300

    Trying to fix build

--- gui/main.rs
+++ gui/main.rs
@@ -7,9 +7,12 @@ extern crate find_folder;
 
 use translator::Translator;
 
-use piston_window::{PistonWindow, WindowSettings, Glyphs, UpdateEvent};
-use conrod::{Theme, Ui, Canvas, TextBox, Widget, Sizeable, Positionable};
+use piston_window::{PistonWindow, WindowSettings, EventLoop, Glyphs, UpdateEvent, G2d};
+use conrod::{Theme, Canvas, TextBox, Widget, Sizeable, Positionable, Graphics};
 use conrod::color::{self, Colorable};
+
+type Backend = (<G2d<'static> as Graphics>::Texture, Glyphs);
+type Ui = conrod::Ui<Backend>;
 
 fn main() {
     let window: PistonWindow = WindowSettings::new("Hello Conrod", [800, 600])
@@ -18,11 +21,11 @@ fn main() {
                                    .build()
                                    .unwrap();
 
-    let mut ui = {
+    let mut ui: Ui = {
         let assets_path = find_folder::Search::KidsThenParents(3, 5).for_folder("assets").unwrap();
         let font_path = assets_path.join("NotoSans-Regular.ttf");
         let theme = Theme::default();
-        let glyph_cache = Glyphs::new(&font_path, window.factory.borrow().clone());
+        let glyph_cache = Glyphs::new(&font_path, window.factory.clone());
         Ui::new(glyph_cache.unwrap(), theme)
     };
 
@@ -44,7 +47,7 @@ fn main() {
                     .pad(30.)
                     .color(background_color)
                     .scroll_kids()
-                    .set(CANVAS, ui);
+                    .set(CANVAS, &mut ui);
 
                 TextBox::new(&mut original_text)
                     .font_size(20)
@@ -52,7 +55,7 @@ fn main() {
                     .mid_left_of(CANVAS)
                     .color(background_color.invert())
                     .react(|_string: &mut String| {})
-                    .set(INPUT_TEXT, ui);
+                    .set(INPUT_TEXT, &mut ui);
 
                 TextBox::new(&mut translated_text)
                     .font_size(20)
@@ -60,11 +63,11 @@ fn main() {
                     .down_from(INPUT_TEXT, 45.0)
                     .color(background_color.invert())
                     .react(|_string: &mut String| {})
-                    .set(OUTPUT_TEXT, ui);
+                    .set(OUTPUT_TEXT, &mut ui);
             })
         });
 
-        window.draw_2d(|context, graphic| ui.draw_if_changed(context, graphic));
+        window.draw_2d(&event, |context, graphic| ui.draw_if_changed(context, graphic));
 
         translated_text = translator.translate(&original_text);
     }
